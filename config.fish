5045f233fb56f2d4237091266705b6ed7e2084c9
jnavb <jorgenavb@gmail.com>
2018-08-29 20:42:48 +0200

    Add examples and Spotify alias

--- config.fish
+++ config.fish
@@ -1,3 +1,8 @@
+alias song='tizonia --spotify-tracks'
+alias album='tizonia --spotify-album'
+alias artist='tizonia --spotify-artist'
+alias playlist='tizonia --spotify-playlist'
+
 alias sublime='sublime-text.subl'
 alias rm='rm -i'
 function fish_prompt
@@ -7,3 +12,11 @@ if status is-interactive
 and not set -q TMUX
     exec tmux
 end
+
+#MAC
+# set -x ANDROID_HOME /Users/jnavarro/Library/Android/sdk
+# set -x ANDROID_SDK_ROOT /Users/jnavarro/Library/Android/sdk
+# set -x ANDROID_AVD_HOME /Users/jnavarro/.android/avd
+
+#SET PATH$ EXAMPLE IN <(((((º> 
+#set -gx PATH $PATH ~/Git/config/
