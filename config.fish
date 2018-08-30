842eb571d2db2863a6846404e393dabf941b40f5
jnavb <jorgenavb@gmail.com>
2018-08-30 20:30:26 +0200

    Add initial grid

--- config.fish
+++ config.fish
@@ -2,21 +2,37 @@ alias song='tizonia --spotify-tracks'
 alias album='tizonia --spotify-album'
 alias artist='tizonia --spotify-artist'
 alias playlist='tizonia --spotify-playlist'
-
 alias sublime='sublime-text.subl'
 alias rm='rm -i'
+
+alias linux-grid='tmux new-session \; split-window -v \; select-pane -t 0 \;'
+alias mac-grid='tmux new-session \; split-window -h \; select-pane -t 0 \;'
+
 function fish_prompt
     powerline-shell --shell bare $status
 end
+
 if status is-interactive
 and not set -q TMUX
-    exec tmux
+    linux-grid 
+    #mac-grid 
 end
 
-#MAC
+function cs
+	cd $argv
+	tmux run-shell -t 1 "clear & ls"
+end
+
+#function ..
+#	cd ..
+#	tmux run-shell -t 1 "ls"
+#end
+#function cd..
+#	cd ..
+#	tmux run-shell -t 1 "ls"
+#end
+
+#MAC SETUP
 # set -x ANDROID_HOME /Users/jnavarro/Library/Android/sdk
 # set -x ANDROID_SDK_ROOT /Users/jnavarro/Library/Android/sdk
 # set -x ANDROID_AVD_HOME /Users/jnavarro/.android/avd
-
-#SET PATH$ EXAMPLE IN <(((((º> 
-#set -gx PATH $PATH ~/Git/config/
