a943bdf8ad6d3f54276e9439eb95d77c8021361f
jnavb <jorgenavb@gmail.com>
2018-12-26 17:27:00 +0100

    Add macmini config

---
+++ mac/config.fish
@@ -0,0 +1,14 @@
+alias rm='rm -i'
+alias mac-grid='tmux new-session \; split-window -v \; select-pane -t 0 \;'
+
+if status is-interactive
+and not set -q TMUX
+    mac-grid 
+end
+
+function cs
+	cd $argv
+	tmux run-shell -t 1 "clear & ls"
+end
+
+set fish_greeting
